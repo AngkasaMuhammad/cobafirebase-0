9c789dabd4ffe9642971026f9434c89bacafe7f7
AngkasaMuhammad <80026325+AngkasaMuhammad@users.noreply.github.com>
2024-10-26 06:01:36 +0700

    Update uploadkefirebase.js

--- api/uploadkefirebase.js
+++ api/uploadkefirebase.js
@@ -1,3 +1,4 @@
+/*
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
 import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js';
 
@@ -6,8 +7,9 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-
-let writeFloat = async value=>{
+*/
+export default async function handler(req, res) {
+	const value = (Math.random()-.5)*999)
 	try{
 		let setvalue = await set(ref(database, 'aa'), value)
 		console.log('Float written successfully: '+value)
@@ -16,9 +18,4 @@ let writeFloat = async value=>{
 		console.error('Gagal writing float: ', e,)
 		res.status(500).json({ error: 'gagal upload' })
 	}
-}
-
-
-export default async function handler(req, res) {
-	await writeFloat((Math.random()-.5)*999)
 }
