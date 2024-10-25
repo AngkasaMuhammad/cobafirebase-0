d9a35c7f51124bb24529736dfb72725f3a8ac095
AngkasaMuhammad <80026325+AngkasaMuhammad@users.noreply.github.com>
2024-10-25 21:51:35 +0700

    Update randomFloat.js

--- api/randomFloat.js
+++ api/randomFloat.js
@@ -2,15 +2,16 @@
 export default async function handler(req, res) {
     const randomFloat = Math.random(); // Generate a random float
     // Here you can integrate with your database (e.g., Firebase)
-    console.log('Random float:', randomFloat);
 
     // Respond to the request
 	let nama = [randomFloat,'satuuu','dua',]
 	let i = 0
 	let f = ()=>{
-        res.status(200).json({ value: nama[i++]});
+		let val = nama[i++]
+		console.log('Random valueeee:', val);
+		res.status(200).json({ value: val});
 	}
+	f()
     setTimeout(f,1111,)
     setTimeout(f,2222,)
-    setTimeout(f,3333,)
 }
